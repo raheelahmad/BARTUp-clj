e7559bda9987bd0bbbf5d829a4742f53e094d8b6
Raheel Ahmad <ahmadraheel@gmail.com>
2017-10-15 19:48:34 -0700

    Clean up project.clj

--- project.clj
+++ project.clj
@@ -27,18 +27,7 @@
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-  ;; uberjar profile is to compile to Java
   :profiles {
-             :uberjar {:aot :all
-                       :hooks [leiningen.cljsbuild]
-                       :cljsbuild {:builds {:prod
-                                            {:source-paths ["src/cljs"]
-                                             :compiler {:main bart.core
-                                                        :output-to "resources/public/js/app.js"
-                                                        :optimizations :advanced
-                                                        :asset-path "js/out"
-                                                        :pretty-print false
-                                                        }}}}}
              :dev {:dependencies [[figwheel-sidecar "0.5.13"]
                                   [com.cemerick/piggieback "0.2.2"] ;; needed for nREPL
                                   [binaryage/dirac "1.2.16"]
@@ -52,15 +41,5 @@
                                        :output-to "resources/public/js/app.js"
                                        :output-dir  "resources/public/js/out"
                                        :asset-path "js/out"
-                                       :source-map-timestamp true}}
-                       :prod {:source-paths ["src/cljs"]
-                              :figwheel false
-                              :compiler {:main bart.core
-                                         :output-to "resources/public/js/app.js"
-                                         :optimizations :advanced
-                                         :asset-path "js/out"
-                                         :pretty-print false
-                                         }}
-                       }
-
+                                       :source-map-timestamp true}}}
               })
