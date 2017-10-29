344cb462348bf3987ad39460ea84b6dd8c6d957b
Raheel Ahmad <ahmadraheel@gmail.com>
2017-10-28 21:52:43 -0700

    Working timeline

--- frontend/cljs/bart/views/timeline.cljs
+++ frontend/cljs/bart/views/timeline.cljs
@@ -14,31 +14,11 @@
 
 ;; Timeline-viz
 
-(def etds-catch [{:direction "North",
-  :lines
-  [{:destination "Richmond",
-    :direction "North",
-    :minutes ["12" "26" "46"]}
-   {:destination "Daly City",
-    :direction "North",
-    :minutes ["21" "41"]}]}
- {:direction "South",
-  :lines
-  [{:destination "Warm Springs",
-    :direction "South",
-    :minutes ["2" "20" "40"]}]}])
-
-(def richmond
-  {:destination "Richmond",
-   :direction "North",
-   :minutes ["12" "26" "46"]})
-
 (defn line-minutes [{:keys [destination minutes direction]}]
   (reduce #(conj %1 {:minutes (if (= direction "South") (- (int %2)) (int %2))
                      :destination destination
                      :direction direction})
-          [] minutes)
-  )
+          [] minutes))
 
 (defn direction-minutes [{:keys [lines direction]}]
   (map line-minutes lines)
@@ -50,63 +30,51 @@
     (concat (->> north (sort-by :minutes))
             ;; (->> south  sort (map -))
             (->> south (sort-by :minutes))
-            )
-    ))
+            )))
 
 (defn all-minutes [etds]
-  (-> etds north-south-minutes flatten)
-  )
+  (-> etds north-south-minutes flatten))
 
 (defn y-range
   [etds view-state]
   (let [minutes (map :minutes (flatten (north-south-minutes etds)))
         max-minute (apply max minutes)
         min-minute (apply min minutes)
-        height (get-height view-state)
-        ]
-    ;; (println (str "All minutes: " minutes))
-    ;; (println (str "max: " max-minute ", min: " min-minute))
+        height (get-height view-state)]
     (-> js/d3
         .scaleLinear
         (.domain #js [min-minute max-minute])
         (.range #js [0 height])
         )))
 
-(defn timeline-enter [app-state view-state]
+(defn timeline-enter [app-state]
   (let [etds (:etds app-state)
-        minutes (all-minutes etds) ]
-    (println (str "Minutes data for enter: " minutes))
+        minutes (all-minutes etds)]
     (-> (js/d3.select ".timeline")
         (.selectAll "rect")
         (.data (clj->js minutes))
         .enter
         (.append "rect")
+        (.attr "x" 10)
+        (.attr "height" 5)
+        (.attr "width" 5)
         )
     ))
 
 (defn timeline-update [app-state view-state]
   (let [etds (:etds app-state)
         y-scale (y-range etds view-state)
-        data (all-minutes etds)
-        _ (println (str "y domain" (.domain y-scale)))
-        data-n (count data)
-        _ (println (str data-n  " Minutes data for update: " data))
-        eg-minute (:minutes (last data))
-        _ (println (str "y for " eg-minute ": " (y-scale eg-minute)))
-        ]
+        data (all-minutes etds)]
     (-> (js/d3.select ".timeline")
         (.selectAll "rect")
-        (.data (clj->js data))
-        (.attr "x" 10)
-        (.attr "y" (fn [d _] (y-scale (:minutes d))))
-        (.attr "height" 5)
-        (.attr "width" 5)
-        )))
+        (.attr "y" (fn [d _]
+                     (y-scale (.-minutes d))
+                     )))))
 
 (defn timeline-exit [app-state]
   (println "Exiting timeline")
   (let [etds (:etds app-state)
-        data (north-south-minutes etds)]
+        data (all-minutes etds)]
     (-> (js/d3.select ".timeline")
         (.selectAll "rect")
         (.data (clj->js data))
@@ -115,7 +83,7 @@
         )))
 
 (defn timeline-did-update [app-state]
-  (timeline-enter app-state @viz-state)
+  (timeline-enter app-state)
   (timeline-update app-state @viz-state)
   (timeline-exit app-state)
   )
