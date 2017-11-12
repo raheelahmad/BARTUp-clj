45677447380e1f65511e61b0b5c350a6051d85b8
Raheel Ahmad <ahmadraheel@gmail.com>
2017-11-12 10:17:24 -0800

    TODO + deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Cleaning"
 lein clean
+echo "Building CLJS for production"
 lein cljsbuild once prod
+echo "Committing app.js"
 git add resources/public/js/app.js
 git commit -m 'Compile JS'
+echo "Pushing to origin"
 git push origin
